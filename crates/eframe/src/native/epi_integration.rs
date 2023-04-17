385f707dd07c32b66d168a90e31363b3a9238293
mbroo <mbrooksr@gmail.com>
2023-04-17 15:34:23 -0400

    idk

--- crates/eframe/src/native/epi_integration.rs
+++ crates/eframe/src/native/epi_integration.rs
@@ -289,7 +289,7 @@ pub fn handle_app_output(
 
     if let Some(mouse_passthrough) = mouse_passthrough {
         println!("setting passthru {}", mouse_passthrough);
-        window.set_cursor_hittest(!mouse_passthrough);
+        window.set_cursor_hittest(mouse_passthrough);
     }
 }
 

a6b8d37f02c0177a4f3bd1284ed1802ca63f483d
mbroo <mbrooksr@gmail.com>
2023-04-17 15:04:36 -0400

    cursor hittest fix

--- crates/eframe/src/native/run.rs
+++ crates/eframe/src/native/run.rs
@@ -700,9 +700,9 @@ mod glow_integration {
             integration.egui_ctx.set_visuals(theme.egui_visuals());
 
             gl_window.window().set_ime_allowed(true);
-            if self.native_options.mouse_passthrough {
-                gl_window.window().set_cursor_hittest(false).unwrap();
-            }
+            // if self.native_options.mouse_passthrough {
+            //     gl_window.window().set_cursor_hittest(false).unwrap();
+            // }
 
             {
                 let event_loop_proxy = self.repaint_proxy.clone();
@@ -825,6 +825,7 @@ mod glow_integration {
                 }
 
                 integration.post_present(window);
+                window.set_cursor_hittest(!self.native_options.mouse_passthrough).unwrap();
 
                 #[cfg(feature = "__screenshot")]
                 // give it time to settle:
